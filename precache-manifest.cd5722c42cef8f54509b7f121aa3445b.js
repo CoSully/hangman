f5efb1ac14301f00ebed6ab886f00e5023f4093c
Chris OSullivan <CoSully@gmail.com>
2019-06-20 19:21:56 +0100

    Updates

---
+++ precache-manifest.cd5722c42cef8f54509b7f121aa3445b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3224089e6c63926a648470a7dc2df78c",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "5d1b5ca2c7b733defdd0",
+    "url": "/hangman/static/css/main.59b95f24.chunk.css"
+  },
+  {
+    "revision": "ded73dc3830fb2d80c34",
+    "url": "/hangman/static/js/2.b7dd99f8.chunk.js"
+  },
+  {
+    "revision": "5d1b5ca2c7b733defdd0",
+    "url": "/hangman/static/js/main.83a3b9d0.chunk.js"
+  },
+  {
+    "revision": "19ffbe7598942725a8e1",
+    "url": "/hangman/static/js/runtime~main.d9fce119.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman/static/media/6.c80394c2.jpg"
+  }
+]);
